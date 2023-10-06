a6ab6565b49a94a9e7b5d63ca55bda0173d35309
staviasz <erickesc21@hotmail.com>
2023-10-05 21:12:56 -0300

    correção da senha de adm

--- src/middlewares/bankPassword.js
+++ src/middlewares/bankPassword.js
@@ -1,8 +1,9 @@
 const { errorResponse401 } = require('../utils/responses/errorResponse');
+const data = require('../models/bancodedados');
 
 const isAuthorized = (req, res, next) => {
   const { senha_banco } = req.query;
-  const bankPassword = 'Cubos123Bank';
+  const bankPassword = data.banco.senha;
 
   if (!senha_banco || senha_banco !== bankPassword)
     return errorResponse401(res, 'A senha do banco informada é inválida!');
